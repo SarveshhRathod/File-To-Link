2c738611920d629fb6d70b4f86855800d382b0e5
Sarvesh Asatkar <sarveshh.asatkarr@gmail.com>
2026-06-30 00:19:45 +0530

    Update __main__.py

--- Adarsh/__main__.py
+++ Adarsh/__main__.py
@@ -3,6 +3,13 @@ import os
 import sys
 import glob
 import asyncio
+
+try:
+    asyncio.get_running_loop()
+except RuntimeError:
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+
 import logging
 import importlib
 from pathlib import Path
