dff728df179becd0c0f28dee1f3192516d131a25
Sarvesh Asatkar <sarveshh.asatkarr@gmail.com>
2026-06-30 09:04:34 +0530

    Refactor asyncio event loop and logging setup
    Refactor event loop setup and improve logging messages.

--- Adarsh/__main__.py
+++ Adarsh/__main__.py
@@ -3,13 +3,6 @@ import os
 import sys
 import glob
 import asyncio
-
-try:
-    asyncio.get_running_loop()
-except RuntimeError:
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-
 import logging
 import importlib
 from pathlib import Path
@@ -21,6 +14,7 @@ from .server import web_server
 from .utils.keepalive import ping_server
 from Adarsh.bot.clients import initialize_clients
 
+# Logging setup
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
@@ -29,26 +23,33 @@ logging.getLogger("aiohttp").setLevel(logging.ERROR)
 logging.getLogger("pyrogram").setLevel(logging.ERROR)
 logging.getLogger("aiohttp.web").setLevel(logging.ERROR)
 
+# Get files path
 ppath = "Adarsh/bot/plugins/*.py"
 files = glob.glob(ppath)
-StreamBot.start()
-loop = asyncio.get_event_loop()
 
+# Event Loop setup compatible with Python 3.8+
+try:
+    loop = asyncio.get_event_loop()
+except RuntimeError:
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
 
 async def start_services():
     print('\n')
-    print('------------------- Initalizing Telegram Bot -------------------')
+    print('------------------- Starting Telegram Bot -------------------')
+    # Starting bot inside async loop to avoid event loop issues
+    await StreamBot.start()
+    
     bot_info = await StreamBot.get_me()
     StreamBot.username = bot_info.username
     print("------------------------------ DONE ------------------------------")
     print()
-    print(
-        "---------------------- Initializing Clients ----------------------"
-    )
+    print("---------------------- Initializing Clients ----------------------")
     await initialize_clients()
     print("------------------------------ DONE ------------------------------")
     print('\n')
-    print('--------------------------- Importing ---------------------------')
+    print('--------------------------- Importing Plugins ---------------------------')
+    
     for name in files:
         with open(name) as a:
             patt = Path(a.name)
@@ -60,28 +61,33 @@ async def start_services():
             spec.loader.exec_module(load)
             sys.modules["Adarsh.bot.plugins." + plugin_name] = load
             print("Imported => " + plugin_name)
-    if Var.ON_HEROKU:
+            
+    # Keep alive for both Heroku and Render to avoid idling
+    if Var.ON_HEROKU or Var.ON_RENDER:
         print("------------------ Starting Keep Alive Service ------------------")
         print()
         asyncio.create_task(ping_server())
-    print('-------------------- Initalizing Web Server -------------------------')
+        
+    print('-------------------- Initializing Web Server -------------------------')
     app = web.AppRunner(await web_server())
     await app.setup()
-    bind_address = "0.0.0.0" if Var.ON_HEROKU else Var.BIND_ADRESS
+    
+    # Render and Heroku bind to 0.0.0.0
+    bind_address = "0.0.0.0" if (Var.ON_HEROKU or Var.ON_RENDER) else Var.BIND_ADRESS
     await web.TCPSite(app, bind_address, Var.PORT).start()
+    
     print('----------------------------- DONE ---------------------------------------------------------------------')
     print('\n')
-    print('---------------------------------------------------------------------------------------------------------')
     print('---------------------------------------------------------------------------------------------------------')
     print(' follow me for more such exciting bots! https://github.com/adarsh-goel')
     print('---------------------------------------------------------------------------------------------------------')
     print('\n')
     print('----------------------- Service Started -----------------------------------------------------------------')
-    print('                        bot =>> {}'.format((await StreamBot.get_me()).first_name))
+    print('                        bot =>> {}'.format(bot_info.first_name))
     print('                        server ip =>> {}:{}'.format(bind_address, Var.PORT))
     print('                        Owner =>> {}'.format((Var.OWNER_USERNAME)))
-    if Var.ON_HEROKU:
-        print('                        app runnng on =>> {}'.format(Var.FQDN))
+    if Var.ON_HEROKU or Var.ON_RENDER:
+        print('                        app running on =>> {}'.format(Var.URL))
     print('---------------------------------------------------------------------------------------------------------')
     print('Give a star to my repo https://github.com/adarsh-goel/filestreambot-pro  also follow me for new bots')
     print('---------------------------------------------------------------------------------------------------------')
